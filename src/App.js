055fd0610eaf9fa15ad52add02f41b1f1c6e9659
ArmanMesropyan <81559732+ArmanMesropyan@users.noreply.github.com>
2022-03-06 14:34:45 +0400

    upload assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
+
+//=============================================
+import AboutPage from './components/content/aboutPage/aboutPage';
+import HomePage from './components/content/homePage/homePage';
+import ServicesPage from './components/content/servicesPage/servicesPage';
+import Team from './components/content/team/team';
+import Footer from './components/footer/footer';
+import Header from './components/header/header';
+import JoinOurTeam from './components/content/joinOurTeam/joinOurTeam';
+import BecomeAPartner from './components/content/becomeaPartner/becomeAPartner';
+import Blog from './components/content/blog/blog';
+import CalculatorPage from './components/content/calculatorPage/calculatorPage';
+import Libary from './components/content/libary/libary';
+
+
+
+const  App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+       
+      <Routes>
+        <Route path='/' element= {<HomePage/>}/>
+        <Route path='/about' element={<AboutPage/>}/>
+        <Route path='/services' element={<ServicesPage/>}/>
+        <Route path='/calculator/*' element={<CalculatorPage/>}/>
+        <Route path='/libary' element={<Libary/>}/>
+        <Route path='/team' element={<Team/>}/>
+        <Route path='/contactUs' element={<Blog/>}/>
+
+        <Route path='/joinTeam' element={<JoinOurTeam/>}/>
+        <Route path='/BecomePartner' element={<BecomeAPartner/>}/>
+
+      
+       
+        
+        
+      </Routes>
+      
+       <hr/>
+      <Footer/>
     </div>
   );
 }
